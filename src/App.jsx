4f87052b269f376b5743819dc06be00778db0162
Egor Okishev <e-okishev@mail.ru>
2023-02-21 22:58:37 +0300

    Create react app and added header

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Container from "./components/Container/Container";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <nav>
+          <Container className="navigation__container" />
+        </nav>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
