2779d611b4f2a076adcf44e85394b0f0f78915dc
Egor Okishev <e-okishev@mail.ru>
2023-02-26 12:17:17 +0300

    Added interactivity to menu items

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,29 @@
 import classNames from "classnames";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Container from "../Container/Container";
 import style from "./Navigation.module.css";
+import { changeCategory } from "../../store/category/categorySlice";
 
 function Navigation() {
   const { category, activeCategory } = useSelector((state) => state.category);
+  const dispatch = useDispatch();
 
   return (
     <nav className={style.navigation}>
       <Container className={style.container}>
         <ul className={style.list}>
           {category.map((item, i) => (
-            <li className={style.item}>
+            <li key={i} className={style.item}>
               <button
                 className={classNames(
                   style.button,
-                  activeCategory === i ? style.button_active : '')}>
+                  activeCategory === i ? style.button_active : ""
+                )}
+                style={{ backgroundImage: `url(${item.image})` }}
+                onClick={() => {
+                  dispatch(changeCategory({indexCategory: i}));
+                }}
+              >
                 {item.rus}
               </button>
             </li>
